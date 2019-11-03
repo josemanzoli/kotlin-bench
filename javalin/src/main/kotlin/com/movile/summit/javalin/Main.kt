3c01e2e94be3cd98a7679f6dea4d91d4454e18a4
Jose Manzoli <jose.manzoli@3420-NB.local>
2019-11-03 20:07:39 -0300

    icluding javalin files

---
+++ javalin/src/main/kotlin/com/movile/summit/javalin/Main.kt
@@ -0,0 +1,27 @@
+package com.movile.summit.javalin
+
+import com.movile.summit.javalin.model.User
+import com.google.gson.Gson
+import io.javalin.Javalin
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+
+fun main() {
+  val app = Javalin.create().start(8080)
+  val gson = Gson()
+
+  val users = ConcurrentHashMap<String, User>()
+
+  app.post("/users") { ctx ->
+    val uuid = UUID.randomUUID().toString()
+    val parsed: User = gson.fromJson<User>(ctx.body(), User::class.java)
+    users[uuid] = parsed
+    ctx.result(uuid)
+  }
+
+  app.get("/users/:id") { ctx ->
+    val user = users.get(ctx.pathParam("id"))
+    ctx.json(user.toString())
+  }
+}
---
+++ javalin/src/main/kotlin/com/movile/summit/javalin/model/User.kt
@@ -0,0 +1,3 @@
+package com.movile.summit.javalin.model
+
+data class User(val id: String, val username: String, val password: String)
