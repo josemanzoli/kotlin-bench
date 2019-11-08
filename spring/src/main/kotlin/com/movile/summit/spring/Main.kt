2eec7868e8950bbdc7c89f1f4b0d7680a0039bef
Jose Manzoli <jose.manzoli@3420-NB.local>
2019-11-07 21:21:20 -0300

    including gson as javalin and spark

--- spring/src/main/kotlin/com/movile/summit/spring/Main.kt
+++ spring/src/main/kotlin/com/movile/summit/spring/Main.kt
@@ -1,9 +1,10 @@
 package com.movile.summit.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = arrayOf(JacksonAutoConfiguration::class))
 class Main
 
 fun main(args: Array<String>) {
